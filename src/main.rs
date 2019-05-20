54076d2abb199ba10ec6fb12d8101a8d0a0c40e7
Colin Kinloch <colin@kinlo.ch>
2019-05-20 15:40:10 +0100

    Thread dft

--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,24 @@ use std::collections::VecDeque;
 use std::sync::atomic;
 
 const F_SIZE: usize = 512;
-const HEIGHT: usize = 1080;
+const HEIGHT: usize = 720;
 const H_SCALE: f64 = 2.;
 const F_SCALE: f64 = 3.;
 
 type InputPort  = jack::InputPortHandle<jack::DefaultAudioSample>;
 
 struct Connector {
-    input: InputPort,
-    samples: Arc<Mutex<VecDeque<f32>>>,
+  input: InputPort,
+  samples: Arc<Mutex<VecDeque<f32>>>,
 }
 
 impl Connector {
-    pub fn new(input: InputPort, samples: Arc<Mutex<VecDeque<f32>>>) -> Self {
-        Connector {
-            input,
-            samples,
-        }
+  pub fn new(input: InputPort, samples: Arc<Mutex<VecDeque<f32>>>) -> Self {
+    Connector {
+      input,
+      samples,
     }
+  }
 }
 
 impl jack::ProcessHandler for Connector {
@@ -49,7 +49,7 @@ impl jack::ProcessHandler for Connector {
 }
 
 fn as_c32_mut(slice: &mut [f32]) -> &mut [c32] {
-    unsafe { std::slice::from_raw_parts_mut(slice.as_mut_ptr() as *mut _, slice.len() / 2) }
+  unsafe { std::slice::from_raw_parts_mut(slice.as_mut_ptr() as *mut _, slice.len() / 2) }
 }
 
 fn interp_colours(colours: &[[f32; 3]], value: f32) -> Vec<f32> {
@@ -63,142 +63,160 @@ fn interp_colours(colours: &[[f32; 3]], value: f32) -> Vec<f32> {
   //low_colour.clone()
 }
 
-fn build_ui(application: &gtk::Application, running: Arc<atomic::AtomicBool>, samples: Arc<Mutex<VecDeque<f32>>>) {
-    let window = gtk::ApplicationWindow::new(application);
+fn build_ui(application: &gtk::Application, running: Arc<atomic::AtomicBool>, frequencies: Arc<Mutex<VecDeque<f32>>>) {
+  let window = gtk::ApplicationWindow::new(application);
 
-    window.set_title("Jack Fourier");
-    window.set_border_width(10);
-    window.set_position(gtk::WindowPosition::Center);
-    window.set_default_size((F_SIZE as f64) as i32, HEIGHT as i32);
+  window.set_title("Jack Fourier");
+  window.set_border_width(10);
+  window.set_position(gtk::WindowPosition::Center);
+  window.set_default_size((F_SIZE as f64) as i32, HEIGHT as i32);
 
-    let drawing_area = gtk::DrawingArea::new();
-    //drawing_area.set_size_request(F_SIZE as i32, HEIGHT as i32);
-    
-    {
-      let raster = RefCell::new(cairo::ImageSurface::create(cairo::Format::Rgb24, F_SIZE as i32, HEIGHT as i32).unwrap());
-      let counter = RefCell::new(0);
-      let plan = Plan::new(Operation::Inverse, F_SIZE);
-      let colour_set = [
-        [0.0, 0.0, 0.0], // Black
-        [1.0, 0.0, 0.0], // Blue
-        [1.0, 1.0, 0.0], // 
-        [0.0, 1.0, 1.0], // 
-        [0.0, 0.0, 1.0], // Blue
-        [1.0, 1.0, 1.0]
-      ];
-      drawing_area.connect_draw(move |drawing_area, cr| {
-        let da_alloc = drawing_area.get_allocation();
-        let scale_height = (f64::from(da_alloc.height) / H_SCALE) as usize;
-        {
-          let mut raster = raster.borrow_mut();
-          let stride = raster.get_stride() as usize;
-          // let height = raster.get_height() as usize;
-          let width = raster.get_width() as usize;
-          let mut d = raster.get_data().unwrap();
+  let drawing_area = gtk::DrawingArea::new();
+  //drawing_area.set_size_request(F_SIZE as i32, HEIGHT as i32);
+  
+  {
+    let raster = RefCell::new(cairo::ImageSurface::create(cairo::Format::Rgb24, F_SIZE as i32, HEIGHT as i32).unwrap());
+    let counter = RefCell::new(0);
+    let colour_set = [
+      [0.0, 0.0, 0.0], // Black
+      [1.0, 0.0, 0.0], // Blue
+      [1.0, 1.0, 0.0], // 
+      [0.0, 1.0, 1.0], // 
+      [0.0, 0.0, 1.0], // Blue
+      [1.0, 1.0, 1.0]
+    ];
+    drawing_area.connect_draw(move |drawing_area, cr| {
+      let da_alloc = drawing_area.get_allocation();
+      let scale_height = (f64::from(da_alloc.height) / H_SCALE) as usize;
+      {
+        let mut raster = raster.borrow_mut();
+        let stride = raster.get_stride() as usize;
+        let height = raster.get_height() as usize;
+        let width = raster.get_width() as usize;
+        let mut d = raster.get_data().unwrap();
 
-          // let mut data = d.deref_mut();
-          
-          let mut samples = samples.lock().unwrap();
-          
-          //let plan = plan.borrow();
-          while samples.len() > F_SIZE {
-            let s = samples.drain(0..F_SIZE);
-            let mut data = s.collect::<Vec<_>>(); // vec![0.0; 32];
-            //let mut data = vec![0.0; i.len()];
-            // data.clone_from_slice(&i[0..32]);
-            dft::transform(as_c32_mut(&mut data), &plan);
-            
-            let mut counter = counter.borrow_mut();
-            *counter += 1;
-            
-            let mut m = 0.0_f32;
-            const BBP: usize = 4;
-            for (i, &s) in (0..width).zip(data.iter()) {
-              //for j in 0..(height/2) {
-              {
-                let j = *counter % scale_height as usize;
-                let p = stride*j;
-                let g = s.abs() * F_SIZE as f32;
-                m = m.max(g);
-                //println!("{} = {}", s, g);
-                let mut ic = interp_colours(&colour_set, (g / 8.).sqrt());
-                // [B, G, R]
-                //let mut ic = interp_colours(&[[0.0, 1.0, 1.0]], g.sqrt());
-                //let g = g.sqrt() * 255.;
-                for c in ic.iter_mut() { *c *= 255. };
-                let iic = ic.iter().map(|v| if *v > 255. {255} else {*v as u8}).collect::<Vec<_>>();
-                for (j, c) in iic.iter().enumerate() {
-                  d[p+i*BBP+j] = *c;
-                }
-                d[p+i*BBP+3] = 0;
-              // d[BBP*j+2] = 0;
-              }
-            }
-            //println!("{}", m);
-          }
-        }
-        cr.scale(F_SCALE, H_SCALE);
+        // let mut data = d.deref_mut();
         
-        //raster.borrow().set_device_scale(5.0, 1.0);
-        cr.set_source_surface(&*raster.borrow(), 0., 0.);
-        //cr.set_source_rgba(1.0, 0.0, 1.0, 1.0);
-        // cr.set_line_width(100.0);
-        cr.rectangle(0.0, 0.0, F_SIZE as f64, HEIGHT as f64);
-        cr.fill();
-        Inhibit(false)
-      });
-    }
-    {
-      let drawing_area = drawing_area.clone();
-      idle_add(move || {
-        drawing_area.queue_draw();
-        Continue(running.load(atomic::Ordering::SeqCst))
-      });
-    }
+        let mut frequencies = frequencies.lock().unwrap();
+        
+        //let plan = plan.borrow();
+        while frequencies.len() > F_SIZE {
+          let mut counter = counter.borrow_mut();
+          *counter += 1;
+          
+          let mut m = 0.0_f32;
+          const BBP: usize = 4;
+          for (i, s) in (0..width).zip(frequencies.drain(0..F_SIZE)) {
+            //for j in 0..(height/2) {
+            {
+              let j = *counter % scale_height.min(height) as usize;
+              let p = stride*j;
+              let g = s.abs() * F_SIZE as f32;
+              m = m.max(g);
+              //println!("{} = {}", s, g);
+              let mut ic = interp_colours(&colour_set, (g / 2.).sqrt());
+              // [B, G, R]
+              //let mut ic = interp_colours(&[[0.0, 1.0, 1.0]], g.sqrt());
+              //let g = g.sqrt() * 255.;
+              for c in ic.iter_mut() { *c *= 255. };
+              let iic = ic.iter().map(|v| if *v > 255. {255} else {*v as u8}).collect::<Vec<_>>();
+              for (j, c) in iic.iter().enumerate() {
+                d[p+i*BBP+j] = *c;
+              }
+              d[p+i*BBP+3] = 0;
+            // d[BBP*j+2] = 0;
+            }
+          }
+          //println!("{}", m);
+        }
+      }
+      cr.scale(F_SCALE, H_SCALE);
+      
+      //raster.borrow().set_device_scale(5.0, 1.0);
+      cr.set_source_surface(&*raster.borrow(), 0., 0.);
+      //cr.set_source_rgba(1.0, 0.0, 1.0, 1.0);
+      // cr.set_line_width(100.0);
+      cr.rectangle(0.0, 0.0, F_SIZE as f64, HEIGHT as f64);
+      cr.fill();
+      Inhibit(false)
+    });
+  }
+  {
+    let drawing_area = drawing_area.clone();
+    idle_add(move || {
+      drawing_area.queue_draw();
+      Continue(running.load(atomic::Ordering::SeqCst))
+    });
+  }
 
-    window.add(&drawing_area);
+  window.add(&drawing_area);
 
-    window.show_all();
+  window.show_all();
 }
 
 fn main() {
-    let mut running = Arc::new(atomic::AtomicBool::new(true));
+  let running = Arc::new(atomic::AtomicBool::new(true));
 
-    let mut jack_client =
-        jack::Client::open("jack_fourier", jack::options::NO_START_SERVER).unwrap().0;
+  let mut jack_client =
+      jack::Client::open("jack_fourier", jack::options::NO_START_SERVER).unwrap().0;
 
-    println!("client created named: {}", jack_client.get_name());
+  println!("client created named: {}", jack_client.get_name());
 
-    let input = jack_client.register_input_audio_port("input").unwrap();
+  let input = jack_client.register_input_audio_port("input").unwrap();
 
-    let samples = Arc::new(Mutex::new(VecDeque::new()));
+  let samples = Arc::new(Mutex::new(VecDeque::new()));
+  let frequencies = Arc::new(Mutex::new(VecDeque::new()));
 
-    let handler = Connector::new(input, samples.clone());
-    jack_client.set_process_handler(handler).unwrap();
-    
-    let application = gtk::Application::new("org.kinloch.colin.jack_fourier",
-                                            Default::default())
-                                       .expect("Initialization failed...");
+  {
+    use std::thread;
+    use std::time::Duration;
+    let running = running.clone();
+    let samples = samples.clone();
+    let frequencies = frequencies.clone();
+    thread::spawn(move || {
+      let plan = Plan::new(Operation::Inverse, F_SIZE);
+      while running.load(atomic::Ordering::SeqCst) {
+        thread::sleep(Duration::from_millis(25));
+        let mut samples = samples.lock().unwrap();
+        let mut frequencies = frequencies.lock().unwrap();
+        while samples.len() > F_SIZE {
+          let mut data = samples.drain(0..F_SIZE).collect::<Vec<_>>();
+          //let data = s.collect::<Vec<_>>(); // vec![0.0; 32];
+          //let mut data = vec![0.0; i.len()];
+          // data.clone_from_slice(&i[0..32]);
+          dft::transform(as_c32_mut(&mut data), &plan);
+          frequencies.extend(data.iter());
+        }
+      }
+    });
+  }
 
+  let handler = Connector::new(input, samples.clone());
+  jack_client.set_process_handler(handler).unwrap();
   
-    // start everything up
-    jack_client.activate().unwrap();
+  let application = gtk::Application::new("org.kinloch.colin.jack_fourier",
+                                          gio::ApplicationFlags::NON_UNIQUE)
+                                     .expect("Initialization failed...");
 
-    let jack_client = Arc::new(Mutex::new(jack_client));
-    {
-      let running = running.clone();
-      application.connect_activate(move |app| {
-        build_ui(app, running.clone(), samples.clone());
-      });
-    }
-    {
-      let running = running.clone();
-      application.connect_shutdown(move |_app| {
-        running.store(false, atomic::Ordering::SeqCst);
-        jack_client.lock().unwrap().close().unwrap();
-      });
-    }
 
-    application.run(&std::env::args().collect::<Vec<_>>());
-    
+  // start everything up
+  jack_client.activate().unwrap();
+
+  let jack_client = Arc::new(Mutex::new(jack_client));
+  {
+    let running = running.clone();
+    let frequencies = frequencies.clone();
+    application.connect_activate(move |app| {
+      build_ui(app, running.clone(), frequencies.clone());
+    });
+  }
+  {
+    let running = running.clone();
+    application.connect_shutdown(move |_app| {
+      running.store(false, atomic::Ordering::SeqCst);
+      jack_client.lock().unwrap().close().unwrap();
+    });
+  }
+
+  application.run(&std::env::args().collect::<Vec<_>>());
 }
